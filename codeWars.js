e343bd20b40e59277913fb663e57a2b4b63b88cf
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-24 09:55:30 -0600

    trouble

--- codeWars.js
+++ codeWars.js
@@ -113,8 +113,18 @@ function repeatStr (n, s) {
 }
 
 //-Question-
-//-Answer-
+function trouble(x, t){
 
+}
+//-Answer-
+function trouble(x, t){
+  for(var i = x.length - 1; i > 0; i--){
+    if(x[i] + x[i - 1] === t){
+    x.splice(i, 1);
+    }
+  }
+    return x;
+}
 //-Question-
 //-Answer-
 
