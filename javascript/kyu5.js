b3db7636dd7764e5721f505c9a34464689d3aebd
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-05 21:41:53 -0700

    direction reduction

--- javascript/kyu5.js
+++ javascript/kyu5.js
@@ -226,8 +226,18 @@ function solution(hour) {
   }
 }
 
-//-Question-
+//-Question- Directions Reduction
 //-Answer-
+function dirReduc(arr){
+  for(let i = 0; i < arr.length; i++){
+    if((arr[i] === 'NORTH' && arr[i + 1] === 'SOUTH') || (arr[i] === 'SOUTH' && arr[i + 1] === 'NORTH') || (arr[i] === 'EAST' && arr[i + 1] === 'WEST') || (arr[i] === 'WEST' && arr[i + 1] === 'EAST')){
+      arr.splice(i + 1, 1);
+      arr.splice(i, 1);
+      i = -1;
+    }
+  }
+  return arr
+}
 
 //-Question-
 //-Answer-
