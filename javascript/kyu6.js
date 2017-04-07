e03f62eedbc9507a79419646d226db1fa0951e0a
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-06 21:29:38 -0700

    fizz/buzz

--- javascript/kyu6.js
+++ javascript/kyu6.js
@@ -859,8 +859,21 @@ function findUniq(arr) {
 }
 
 
-//-Question-
+//-Question- Fizz/Buzz
 //-Answer-
+function solution(number){
+  let result = [0, 0, 0]
+  for(let i = 1; i < number; i++){
+    if(i % 3 === 0 && i % 5 !== 0){
+      result[0] += 1;
+    } else if (i % 5 === 0 && i % 3 !== 0){
+      result[1] += 1;
+    } else if ( i % 3 === 0 && i % 5 === 0){
+      result[2] += 1;
+    }
+  }
+  return result;
+}
 
 //-Question-
 //-Answer-
