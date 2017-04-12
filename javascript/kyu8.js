fb01fa392b2842b6892821c23311ee1c9f664bcf
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-12 14:33:21 -0700

    switch it up

--- javascript/kyu8.js
+++ javascript/kyu8.js
@@ -361,8 +361,32 @@ function simpleMultiplication(n){
   return n % 2 === 0 ? n * 8 : n * 9;
 }
 
-//-Question-
+//-Question-Switch it up;
 //-Answer-
+function switchItUp(number){
+  switch(number){
+    case 0:
+      return 'Zero';
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    case 9:
+      return 'Nine';
+  }
+}
 
 
 //-Question-
